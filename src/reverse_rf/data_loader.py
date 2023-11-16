8c2317160f4abd79474bffa1b0eab6df1b6de171
Sigrun May <sigrun.may@gmail.com>
2023-11-16 17:23:13 +0100

    initial commit

---
+++ src/reverse_rf/data_loader.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021 Sigrun May, Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+# Copyright (c) 2021 Sigrun May, Ostfalia Hochschule für angewandte Wissenschaften
+# Copyright (c) 2020 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""Data loader module."""
+
+from typing import List, Tuple
+
+import joblib
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def load_colon_data() -> Tuple[pd.DataFrame, pd.Series]:
+    """Load colon data.
+
+    The data is loaded and parsed from the internet.
+    Also see <http://genomics-pubs.princeton.edu/oncology/affydata/index.html>
+
+    Returns:
+        Tuple containing labels and data.
+    """
+    cache_data_file = "./data/colon_data.pkl.gz"
+    try:
+        data_df, label = joblib.load(cache_data_file)
+    except:
+        html_data = "http://genomics-pubs.princeton.edu/oncology/affydata/I2000.html"
+
+        page = requests.get(html_data)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        colon_text_data = soup.get_text()
+
+        colon_text_data_lines = colon_text_data.splitlines()
+        colon_text_data_lines = [
+            [float(s) for s in line.split()]
+            for line in colon_text_data_lines
+            if len(line) > 20
+        ]
+        assert len(colon_text_data_lines) == 2000
+        assert len(colon_text_data_lines[0]) == 62
+
+        data = np.array(colon_text_data_lines).T
+
+        html_label = "http://genomics-pubs.princeton.edu/oncology/affydata/tissues.html"
+        page = requests.get(html_label)
+        soup = BeautifulSoup(page.content, "html.parser")
+        colon_text_label = soup.get_text()
+        colon_text_label = colon_text_label.splitlines()
+
+        label = []
+
+        for line in colon_text_label:
+            try:
+                i = int(line)
+                label.append(0 if i > 0 else 1)
+            except:  # noqa: E722
+                pass
+
+        assert len(label) == 62
+
+        data_df = pd.DataFrame(data)
+
+        # generate feature names
+        column_names = []
+        for column_name in data_df.columns:
+            column_names.append("gene_" + str(column_name))
+
+        data_df.columns = column_names
+
+        # cache data
+        # joblib.dump((data_df, label), cache_data_file, compress=("gzip", 3))
+
+    return data_df, pd.Series(label)
+
+
+def load_prostate_data() -> Tuple[pd.DataFrame, pd.Series]:
+    """Load prostate data.
+
+    The data is loaded and parsed from <https://web.stanford.edu/~hastie/CASI_files/DATA/prostate.html>
+
+    Returns:
+        Tuple containing labels and data.
+    """
+    cache_data_file = "./data/prostate_data.pkl.gz"
+    try:
+        data, labels = joblib.load(cache_data_file)
+    except:
+        df = pd.read_csv(
+            "https://web.stanford.edu/~hastie/CASI_files/DATA/prostmat.csv"
+        )
+        data = df.T
+
+        # labels
+        labels = []
+        for label in df.columns:  # pylint:disable=no-member
+            if "control" in label:
+                labels.append(0)
+            elif "cancer" in label:
+                labels.append(1)
+            else:
+                assert False, "This must not happen!"
+
+        assert len(labels) == 102
+        assert data.shape == (102, 6033)
+
+        # cache data
+        # joblib.dump((data, labels), cache_data_file, compress=("gzip", 3))
+
+    return data, pd.Series(labels)
+
+
+def load_leukemia_data() -> Tuple[pd.DataFrame, pd.Series]:
+    """Load leukemia data.
+
+    The data is loaded and parsed from the internet.
+    Also see <https://web.stanford.edu/~hastie/CASI_files/DATA/leukemia.html>
+
+    Returns:
+        Tuple containing labels and data.
+    """
+    cache_data_file = "./data/leukemia_data.pkl.gz"
+    try:
+        data, labels = joblib.load(cache_data_file)
+    except:
+        df = pd.read_csv(
+            "https://web.stanford.edu/~hastie/CASI_files/DATA/leukemia_big.csv"
+        )
+        data = df.T
+
+        # labels
+        labels = []
+        for label in df.columns:  # pylint:disable=no-member
+            if "ALL" in label:
+                labels.append(0)
+            elif "AML" in label:
+                labels.append(1)
+            else:
+                assert False, "This must not happen!"
+
+        assert len(labels) == 72
+        assert data.shape == (72, 7128)
+
+        # cache data
+        # joblib.dump((data, labels), cache_data_file, compress=("gzip", 3))
+
+    return data, pd.Series(labels)
+
+
+def standardize_sample_size(data, label) -> Tuple[pd.DataFrame, pd.Series]:
+    """Reduce samples to 15 for each class.
+
+    Returns:
+        Tuple containing balanced data and corresponding labels.
+    """
+    # reduce samples to 15 for each class
+    indices: List[int] = []
+    for i in range(label.shape[0]):
+        if label.iloc[i] == 0:
+            indices.append(i)
+        if len(indices) == 15:
+            break
+
+    for i in range(label.shape[0]):
+        if label.iloc[i] == 1:
+            indices.append(i)
+        if len(indices) == 30:
+            break
+
+    data = data.iloc[indices]
+    label = label.iloc[indices]
+    assert data.shape[0] == 30
+    assert len(label) == 30
+
+    print(data.shape)
+    print(label.shape)
+
+    return data, label
---
+++ src/reverse_rf/standard_rf.py
@@ -0,0 +1,53 @@
+import optuna
+from optuna.samplers import TPESampler, QMCSampler
+import warnings
+import math
+from sklearn.ensemble import RandomForestClassifier
+
+warnings.filterwarnings("ignore")
+# import optuna_study_pruner
+
+
+def optimize(train_index, data_df, meta_data):
+    def optuna_objective(trial):
+        rf_clf = RandomForestClassifier(
+            warm_start=False,
+            max_features=None,
+            oob_score=True,
+            max_depth=trial.suggest_int("max_depth", 1, 20),
+            # n_estimators=30,
+            random_state=42,
+            min_samples_leaf=trial.suggest_int(
+                "min_samples_leaf", 2, math.floor(len(train_index) / 2)
+            ),
+        )
+        rf_clf.fit(data_df.iloc[train_index, 1:], data_df.loc[train_index, "label"])
+        score = rf_clf.oob_score_
+        print(score)
+        return score
+
+    study = optuna.create_study(
+        # storage = "sqlite:///optuna_test.db",
+        # load_if_exists = True,
+        direction="maximize",
+        sampler=TPESampler(
+            # multivariate=True,
+            seed=42,
+        ),
+    )
+    if meta_data["parallel"]["cluster"]:  # deactivate logging on cluster
+        optuna.logging.set_verbosity(optuna.logging.ERROR)
+
+    # terminator = TerminatorCallback()
+    study.optimize(
+        optuna_objective,
+        # n_trials=meta_data["selection_method"]["reverse_trees"]["trials"],
+        n_trials=15,
+        # callbacks=[terminator],
+        timeout=120,
+    )
+
+    clf = RandomForestClassifier()
+    clf.set_params(**study.best_params)
+    clf.fit(data_df.iloc[train_index, 1:], data_df.loc[train_index, "label"])
+    return clf.feature_importances_
