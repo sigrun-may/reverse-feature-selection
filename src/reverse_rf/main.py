f7908680117070fe49ecc32bda229c4bcd4865bf
Sigrun May <sigrun.may@gmail.com>
2023-12-01 20:52:08 +0100

    Proof of concept.

--- src/reverse_rf/main.py
+++ src/reverse_rf/main.py
@@ -16,31 +16,29 @@ import toml
 with open("../../settings.toml", "r") as file:
     meta_data = toml.load(file)
 
+print(type(meta_data))
 
 # data_df = pd.read_csv(
 #     "/home/sigrun/PycharmProjects/ensemble-feature-selection-benchmark/data/overlapping/overlapping.csv"
 # ).iloc[:, :500]
 # assert data_df.columns[0] == 'label'
 
-data_df = pd.read_csv("/home/sma19/PycharmProjects/reverse_feature_selection/data/small_50.csv")
-assert data_df.columns[0] == "label"
-
-
-# joblib.dump(data_df.columns, f"{data_name}_feature_names.pkl")
+# data_df = pd.read_csv(meta_data["data"]["path"])
+# assert data_df.columns[0] == "label"
 
 # # shorten artificial data for faster testing
 # data_01 = data_df.iloc[:, 0:10]
 # data_02 = data_df.iloc[:, 200:800]
 # data_df = pd.concat([data_01, data_02], join="outer", axis=1)
 
-# # data loaders
-# import data_loader
-# X, y = data_loader.standardize_sample_size(*data_loader.load_colon_data())
-# data_df = pd.DataFrame(X)
-# data_df.insert(loc=0, column="label", value=y)
-# # data_df.columns = data_df.columns.astype(str)
-# data_df = data_df.reset_index(drop=True)
-# assert data_df.columns[0] == "label"
+# data loaders
+import data_loader
+X, y = data_loader.standardize_sample_size(*data_loader.load_colon_data())
+data_df = pd.DataFrame(X)
+data_df.insert(loc=0, column="label", value=y)
+# data_df.columns = data_df.columns.astype(str)
+data_df = data_df.reset_index(drop=True)
+assert data_df.columns[0] == "label"
 
 print(data_df.shape)
 
@@ -105,9 +103,20 @@ end_time = datetime.datetime.utcnow()
 print(end_time - start_time)
 
 method_result_dict = {"rf": cv_result_list, "rf_standard_validation_score": standard_validation_score_list}
+
+print(type(meta_data))
+
+# save results
 result_dict = {
     "meta_data": meta_data,
     "method_result_dict": method_result_dict,
     "indices": cv_indices_list,
 }
+
+with open(
+        f"../../results/{meta_data['experiment_id']}_result_dict2.pkl", "wb"
+) as file:
+    pickle.dump(result_dict, file, protocol=pickle.HIGHEST_PROTOCOL)
+
 joblib.dump(result_dict, f"../../results/{meta_data['experiment_id']}_result_dict.pkl")
+
