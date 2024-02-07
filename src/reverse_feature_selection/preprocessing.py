110235313dc3124821ebdab0b292a70fd822ee6e
Sigrun May <sigrun.may@gmail.com>
2024-02-07 11:56:10 +0100

    Remove caching training data. Save raw oob error lists.

--- src/reverse_feature_selection/preprocessing.py
+++ src/reverse_feature_selection/preprocessing.py
@@ -24,59 +24,31 @@ import numpy as np
 import pandas as pd
 
 
-def load_cached_data(
-    pickle_base_path: Path, correlation_matrix: bool
-) -> Tuple[pd.DataFrame, pd.DataFrame, Optional[pd.DataFrame]]:
-    """
-    Load the cached preprocessed data.
-
-    Args:
-        pickle_base_path: The base path for caching the preprocessed data.
-        correlation_matrix: Whether to load the Spearman correlation matrix for the training data.
-
-    Returns:
-        A tuple containing the validation data, training data, and optionally the training correlation matrix.
-    """
-    with open(f"{pickle_base_path}/validation.pkl", "rb") as file:
-        validation_pd = pickle.load(file)
-    with open(f"{pickle_base_path}/train.pkl", "rb") as file:
-        train_pd = pickle.load(file)
-    if correlation_matrix:
-        with open(f"{pickle_base_path}/train_correlation_matrix.pkl", "rb") as file:
-            train_correlation_matrix = pickle.load(file)
-    else:
-        train_correlation_matrix = None
-    return train_pd, validation_pd, train_correlation_matrix
-
-
 def preprocess_data(
     train_index: np.ndarray,
-    validation_index: np.ndarray,
     data_df: pd.DataFrame,
     fold_index: int,
     meta_data: dict,
-    correlation_matrix: bool = False,
 ) -> Tuple[pd.DataFrame, pd.DataFrame, Optional[pd.DataFrame]]:
     """
-    Split data for training and validation, calculate correlation matrix if selected and cache it.
+    Split data for training and validation, calculate Spearman correlation matrix and cache it.
 
     Args:
         train_index: Indices for the training split.
-        validation_index: Indices for the validation split.
         data_df: Complete input data as a Pandas DataFrame.
         fold_index: The current loop iteration of the outer cross-validation.
         meta_data: The metadata related to the dataset and experiment.
-        correlation_matrix: Whether to calculate the Spearman correlation matrix for the training data.
 
     Returns:
-        A tuple containing the validation data, training data, and optionally the training correlation matrix.
+        A tuple containing the validation data, training data, and the training correlation matrix.
     """
     # Create a base path for caching the preprocessed data
     pickle_base_path = Path(f"./preprocessed_data/{meta_data['data']['name']}/outer_fold_{fold_index}")
     # Check if the preprocessed data is already cached
     if pickle_base_path.exists():
         # Load the cached preprocessed data
-        return load_cached_data(pickle_base_path, correlation_matrix)
+        with open(f"{pickle_base_path}/train_correlation_matrix.pkl", "rb") as file:
+            return pickle.load(file)
 
     # Create directory for caching the preprocessed data
     pickle_base_path.mkdir(parents=True, exist_ok=True)
@@ -85,36 +57,21 @@ def preprocess_data(
     # Check for missing values in the input DataFrame
     assert not data_df.isnull().values.any(), "Missing values detected in the input data."
 
-    # Extract the training and validation data based on the provided indices
+    # Extract the training data based on the provided indices
     train_df = data_df.iloc[train_index, :]
-    validation_df = data_df.iloc[validation_index, :]
-
-    # Ensure the shapes of training and validation data match the expected dimensions
-    assert validation_df.shape == (len(validation_index), data_df.shape[1]), "Validation data shape mismatch."
     assert train_df.shape == (len(train_index), data_df.shape[1]), "Training data shape mismatch."
 
-    train_correlation_matrix = None
-    if correlation_matrix:
-        # If specified, calculate the Spearman correlation matrix for the training data
-        unlabeled_train_df = train_df.loc[:, train_df.columns != "label"]  # Exclude the label column
-        assert unlabeled_train_df.shape[0] == len(train_index)
-        assert unlabeled_train_df.shape[1] == data_df.shape[1] - 1  # Exclude the label
+    # Calculate the Spearman correlation matrix for the training data
+    unlabeled_train_df = train_df.loc[:, train_df.columns != "label"]  # Exclude the label column
+    assert unlabeled_train_df.shape[0] == len(train_index)
+    assert unlabeled_train_df.shape[1] == data_df.shape[1] - 1  # Exclude the label
 
-        # Calculate the Spearman correlation matrix
-        train_correlation_matrix = unlabeled_train_df.corr(method="spearman")
-        assert train_correlation_matrix.shape[0] == train_correlation_matrix.shape[1] == unlabeled_train_df.shape[1]
-        if meta_data["pickle_preprocessed_data"]:
-            with open(f"{pickle_base_path}/train_correlation_matrix.pkl", "wb") as file:
-                pickle.dump(train_correlation_matrix, file, protocol=pickle.HIGHEST_PROTOCOL)
-
+    train_correlation_matrix = unlabeled_train_df.corr(method="spearman")
+    assert train_correlation_matrix.shape[0] == train_correlation_matrix.shape[1] == unlabeled_train_df.shape[1]
     if meta_data["pickle_preprocessed_data"]:
-        with open(f"{pickle_base_path}/validation.pkl", "wb") as file:
-            pickle.dump(validation_df, file, protocol=pickle.HIGHEST_PROTOCOL)
-        with open(f"{pickle_base_path}/train.pkl", "wb") as file:
-            pickle.dump(train_df, file, protocol=pickle.HIGHEST_PROTOCOL)
-
-    # Return the training and validation data along with the correlation matrix (if calculated)
-    return train_df, validation_df, train_correlation_matrix
+        with open(f"{pickle_base_path}/train_correlation_matrix.pkl", "wb") as file:
+            pickle.dump(train_correlation_matrix, file, protocol=pickle.HIGHEST_PROTOCOL)
+    return train_correlation_matrix
 
 
 def remove_features_correlated_to_target_feature(
