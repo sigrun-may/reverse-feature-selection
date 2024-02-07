a86a3f84529f1c00be547e46512b82fa36e0e5a1
Sigrun May <sigrun.may@gmail.com>
2024-02-07 07:50:31 +0100

    Check shape

--- src/validation/weighted_manhattan_distance.py
+++ src/validation/weighted_manhattan_distance.py
@@ -7,5 +7,7 @@ class WeightedManhattanDistance:
         self._weights = weights
 
     def __call__(self, p1, p2):
+        assert p1.shape == p2.shape
+        assert p1.shape == self._weights.shape
         dist = np.sum(np.abs(p1 - p2) * self._weights)
         return dist
