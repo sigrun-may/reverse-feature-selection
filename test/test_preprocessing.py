b6613f2f565096f560db02d3d797e487b6d64304
Sigrun May <sigrun.may@gmail.com>
2024-06-18 06:39:29 +0200

    Test reverse feature selection.

---
+++ test/test_preprocessing.py
@@ -0,0 +1,47 @@
+import pytest
+import pandas as pd
+from reverse_feature_selection.preprocessing import remove_features_correlated_to_target_feature
+
+
+def test_removal_of_correlated_features():
+    from mltb2.data import load_colon
+
+    y, x = load_colon()
+    train_df = pd.DataFrame(x)
+    train_df.insert(loc=0, column="label", value=y)
+    train_df.columns = train_df.columns.astype(str)
+    correlation_matrix_df = train_df.corr(method="spearman")
+    target_feature = "1"
+    meta_data = {"train_correlation_threshold": 0.2}
+
+    uncorrelated_train_df = remove_features_correlated_to_target_feature(
+        train_df, correlation_matrix_df, target_feature, meta_data
+    )
+    assert "label" == uncorrelated_train_df.columns[0]
+    assert target_feature not in uncorrelated_train_df.columns
+    uncorrelated_features = uncorrelated_train_df.columns[1:]
+
+    # Check if the maximum correlation of any uncorrelated feature with the target is within the threshold
+    assert (
+        correlation_matrix_df.loc[target_feature, uncorrelated_features].abs().max()
+        <= meta_data["train_correlation_threshold"]
+    ), f"{meta_data['train_correlation_threshold']}"
+
+    # Check if all correlation values left, which are not included in the uncorrelated_features are above the threshold
+    correlated_features = correlation_matrix_df.columns[
+        correlation_matrix_df.loc[target_feature].abs() > meta_data["train_correlation_threshold"]
+    ]
+    assert (
+        correlation_matrix_df.loc[target_feature, correlated_features].abs().min()
+        > meta_data["train_correlation_threshold"]
+    ), f"{meta_data['train_correlation_threshold']}"
+
+
+def test_removal_of_correlated_features_no_features_uncorrelated_to_target_feature():
+    train_df = pd.DataFrame({"label": [1, 0, 1], "feature1": [10, 20, 30], "feature2": [100, 200, 300]})
+    correlation_matrix_df = train_df.corr(method="spearman")
+    target_feature = "feature1"
+    meta_data = {"train_correlation_threshold": 0.6}
+
+    with pytest.raises(ValueError):
+        remove_features_correlated_to_target_feature(train_df, correlation_matrix_df, target_feature, meta_data)
---
+++ test/test_reverse_rf_random.py
@@ -0,0 +1,60 @@
+import pytest
+import pandas as pd
+import numpy as np
+from mltb2.data import load_colon
+
+from reverse_feature_selection.reverse_rf_random import calculate_oob_errors, select_feature_subset
+
+
+def load_test_data():
+    y, x = load_colon()
+    train_df = pd.DataFrame(x)
+    train_df.insert(loc=0, column="label", value=y)
+    train_df.columns = train_df.columns.astype(str)
+
+    # reset index for cross validation splits
+    data_df = train_df.reset_index(drop=True)
+    assert data_df.columns[0] == "label"
+    return train_df
+
+
+def test_calculate_oob_errors_with_valid_input():
+    data_df = load_test_data().iloc[:, :50]
+    meta_data = {"n_cpus": 2, "random_seeds": [0, 1], "train_correlation_threshold": 0.2}
+    labeled_errors, unlabeled_errors = calculate_oob_errors("1", data_df, data_df.corr(method="spearman"), meta_data)
+    assert labeled_errors is not None
+    assert unlabeled_errors is not None
+
+
+def test_calculate_oob_errors_with_invalid_target_feature():
+    data_df = load_test_data().iloc[:, :50]
+    meta_data = {"n_cpus": 2, "random_seeds": [0, 1], "train_correlation_threshold": 0.2}
+    with pytest.raises(KeyError):
+        calculate_oob_errors("invalid_feature", data_df, data_df.corr(), meta_data)
+
+
+def test_calculate_feature_importance_with_invalid_train_indices():
+    data_df = pd.DataFrame(
+        {"label": [1, 0, 1, 0, 1], "feature1": [0.1, 0.2, 0.3, 0.4, 0.5], "feature2": [0.5, 0.4, 0.3, 0.2, 0.1]}
+    )
+    train_indices = np.array([0, 1, 5000000])  # index 5000000 is out of bounds
+    meta_data = {"n_cpus": 2, "random_seeds": [0, 1], "train_correlation_threshold": 0.2}
+    with pytest.raises(IndexError):
+        select_feature_subset(data_df, train_indices, meta_data)
+
+
+def test_calculate_feature_importance_with_no_features():
+    data_df = pd.DataFrame({"label": [1, 0, 1, 0, 1]})
+    train_indices = np.array([0, 1, 2])
+    meta_data = {"n_cpus": 2, "random_seeds": [0, 1], "train_correlation_threshold": 0.2}
+    with pytest.raises(ValueError):
+        select_feature_subset(data_df, train_indices, meta_data)
+
+
+def test_calculate_oob_errors_with_correlated_features_only():
+    data_df = pd.DataFrame(
+        {"label": [1, 0, 1, 0, 1], "feature1": [0.1, 0.2, 0.3, 0.4, 0.5], "feature2": [1, 2, 3, 4, 5]}
+    )
+    meta_data = {"n_cpus": 2, "random_seeds": [0, 1], "train_correlation_threshold": 0.2}
+    with pytest.raises(ValueError):
+        calculate_oob_errors("feature1", data_df, data_df.corr(method="spearman"), meta_data)
