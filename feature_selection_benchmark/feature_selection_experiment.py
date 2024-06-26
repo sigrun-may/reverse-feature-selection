2adeff198c79b9fb2f4975aac690cf273ac7acb1
Sigrun May <sigrun.may@gmail.com>
2024-06-26 06:14:48 +0200

    Add random noise as data input.

--- feature_selection_benchmark/feature_selection_experiment.py
+++ feature_selection_benchmark/feature_selection_experiment.py
@@ -15,10 +15,10 @@ from pathlib import Path
 import cross_validation
 import git
 
-# from standard_rf import optimize as calculate_feature_importance
 from data_loader_tools import load_data_with_standardized_sample_size
-from standard_rf import optimize as calculate_feature_importance
-from reverse_feature_selection.reverse_rf_random import select_feature_subset
+
+# from reverse_feature_selection.reverse_rf_random import select_feature_subset
+# from standard_rf import optimize as calculate_feature_importance
 
 
 def main():
@@ -26,6 +26,7 @@ def main():
     # parse result path from input
     result_base_path = Path(sys.argv[1])
     print("result data_path: ", result_base_path)
+    result_base_path.mkdir(parents=True, exist_ok=True)
 
     # save git commit hash
     git_repository = git.Repo(search_parent_directories=True)
@@ -33,10 +34,13 @@ def main():
 
     # define meta data for the experiment
     meta_data_dict = {
-        "experiment_id": "deleteleukemia_shift_00",
+        "experiment_id": "leukemia_shift_02",
         # valid data names for the data loader are "colon", "prostate" or "leukemia_big"
-        "data_name": "colon",
+        "data_name": "leukemia_big",
         "description": "Leukemia dataset",
+        # "experiment_id": "random_noise_normal_scale_2_30_2000",
+        # "data_name": "random_noise_normal_scale_2_30_2000",
+        # "description": "Random noise dataset for testing purposes with 30 samples and 2000 features.",
         "git_commit_hash": commit_sha,
         "n_cpus": multiprocessing.cpu_count(),  # number of available CPUs
         "train_correlation_threshold": 0.2,
@@ -82,8 +86,8 @@ def main():
     # # shift seeds by 10
     # meta_data_dict["random_seeds"] = [seed + 10 for seed in meta_data_dict["random_seeds"]]
 
-    # # shift seeds by 20
-    # meta_data_dict["random_seeds"] = [seed + 20 for seed in meta_data_dict["random_seeds"]]
+    # shift seeds by 20
+    meta_data_dict["random_seeds"] = [seed + 20 for seed in meta_data_dict["random_seeds"]]
 
     # load previous result for the given experiment, if available
     result_dict_path = Path(f"{result_base_path}/{meta_data_dict['experiment_id']}_result_dict.pkl")
@@ -96,6 +100,18 @@ def main():
     # load data
     data_df = load_data_with_standardized_sample_size(meta_data_dict["data_name"])
 
+    # generate random noise data for benchmarking
+    # import numpy as np
+    # import pandas as pd
+    # rnd = np.random.default_rng(seed=42)
+    # data_df = pd.DataFrame(rnd.normal(scale=2, size=(30, 2000)), columns=[f"f_{i}" for i in range(2000)])
+    # # generate array as binary balanced target with two classes for 30 samples
+    # label_array = np.concatenate([np.zeros(15), np.ones(15)])
+    # data_df.insert(0, "label", label_array)
+    # # save data_df as csv
+    # data_df_path = Path(f"{result_base_path}/{meta_data_dict['experiment_id']}_data_df.csv")
+    # data_df.to_csv(data_df_path, index=False)
+
     # # load artificial data for testing from csv file
     # import pandas as pd
     # data_df = pd.read_csv(
@@ -106,20 +122,22 @@ def main():
 
     print("number of samples", data_df.shape[0], "number of features", data_df.shape[1] - 1)
 
-    # # calculate raw feature subset data for reverse random forest
-    # result_dict["reverse_random_forest"] = cross_validation.cross_validate(
-    #     data_df, meta_data_dict, select_feature_subset
-    # )
-    # result_dict["reverse_random_forest_meta_data"] = meta_data_dict
+    # calculate raw feature subset data for reverse random forest
+    from reverse_feature_selection.reverse_rf_random import select_feature_subset
 
-    # calculate raw feature subset data for standard random forest
-    result_dict["standard_random_forest"] = cross_validation.cross_validate(
-        data_df, meta_data_dict, calculate_feature_importance
+    result_dict["reverse_random_forest"] = cross_validation.cross_validate(
+        data_df, meta_data_dict, select_feature_subset
     )
-    result_dict["standard_random_forest_meta_data"] = meta_data_dict
+    result_dict["reverse_random_forest_meta_data"] = meta_data_dict
+
+    # # calculate raw feature subset data for standard random forest
+    # from standard_rf import calculate_feature_importance
+    # result_dict["standard_random_forest"] = cross_validation.cross_validate(
+    #     data_df, meta_data_dict, calculate_feature_importance
+    # )
+    # result_dict["standard_random_forest_meta_data"] = meta_data_dict
 
     # save results
-    result_base_path.mkdir(parents=True, exist_ok=True)
     result_dict_path = Path(f"{result_base_path}/{meta_data_dict['experiment_id']}_result_dict.pkl")
     with open(result_dict_path, "wb") as file:
         pickle.dump(result_dict, file, protocol=pickle.HIGHEST_PROTOCOL)
