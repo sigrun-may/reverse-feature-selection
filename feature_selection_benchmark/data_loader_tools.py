76473444a718cb5b4e0596fc5e5a974d9aa39c01
Sigrun May <sigrun.may@gmail.com>
2024-08-30 16:08:26 +0200

    Bugfix and cleanup

--- feature_selection_benchmark/data_loader_tools.py
+++ feature_selection_benchmark/data_loader_tools.py
@@ -4,33 +4,34 @@
 
 """Data loader tools."""
 from pathlib import Path
-from typing import Literal
 
 import pandas as pd
 from mltb2.data import load_colon, load_leukemia_big, load_prostate
 
 
-def load_data_with_standardized_sample_size(
-    data: Literal["colon", "prostate", "leukemia_big"] = "colon"
-) -> pd.DataFrame:
-    """Load data with standardized sample size.
-
-    The data is loaded and parsed from the internet (see mltb2.data). The function standardizes the sample size to
-    the first 15 samples of each class.
+def standardize_sample_size_of_hold_out_data(hold_out_data_df: pd.DataFrame, shuffle_seed: int) -> pd.DataFrame:
+    """Standardize the sample size of the hold out data, to ensure balanced classes.
 
     Args:
-        data: Name of the data to load. Possible options are "colon", "prostate", "leukemia_big".
+        hold_out_data_df: The hold out data as a pandas DataFrame with the label in the first column.
+        shuffle_seed: Seed for shuffling.
 
     Returns:
-        Dataframe containing labels and data.
+        The standardized data as a pandas DataFrame with balanced classes: 7 samples for each class.
     """
-    # generate function from string
-    load_data_function = globals()[f"load_{data}"]
+    # shuffle data before selecting samples, if shuffle_seed is set
+    unlabeled_hold_out_data_df, label_series = shuffle(
+        hold_out_data_df.iloc[:, 1:], hold_out_data_df["label"], shuffle_seed
+    )
+    max_samples = 7
+    hold_out_test_indices_0, _ = get_indices_for_class(label_series, 0, max_samples)
+    hold_out_test_indices_1, _ = get_indices_for_class(label_series, 1, max_samples)
 
-    # standardize sample size to balanced data
-    label, data = load_data_function()
-    x, y = standardize_sample_size(data, label)
-    return convert_to_single_df(x, y)
+    hold_out_test_indices = hold_out_test_indices_0 + hold_out_test_indices_1
+    balanced_hold_out_test_data_df = convert_to_single_df(
+        unlabeled_hold_out_data_df.iloc[hold_out_test_indices], label_series.iloc[hold_out_test_indices]
+    )
+    return balanced_hold_out_test_data_df
 
 
 def convert_to_single_df(x, y):
@@ -54,89 +55,125 @@ def convert_to_single_df(x, y):
     return data_df
 
 
-def load_train_test_data_for_standardized_sample_size(
-    data_name: Literal["colon", "prostate", "leukemia_big"] = "colon"
-) -> tuple[pd.DataFrame, pd.DataFrame]:
+def get_indices_for_class(label_series: pd.Series, class_label: int, max_samples: int) -> tuple[list[int], list[int]]:
+    """Get train and test indices for a specific class label.
+
+    Args:
+        label_series: Series containing labels.
+        class_label: The class label to filter.
+        max_samples: Maximum number of samples to include in the train set.
+
+    Returns:
+        Tuple containing train indices and test indices.
+    """
+    # check if class label is in label series
+    assert class_label in label_series.unique(), f"Class label {class_label} not found in label series."
+
+    # check if max_samples is smaller than the number of samples for the class label
+    assert (
+        max_samples < label_series[label_series == class_label].shape[0]
+    ), f"Number of samples for class label {class_label} is smaller than max_samples."
+
+    train_indices = []
+    test_indices = []
+    counter = 0
+
+    for i in range(label_series.shape[0]):
+        if label_series.iloc[i] == class_label:
+            if counter >= max_samples:
+                test_indices.append(i)
+            else:
+                train_indices.append(i)
+            counter += 1
+    assert len(train_indices) == max_samples, f"{len(train_indices)} != {max_samples}"
+    assert len(test_indices) == label_series[label_series == class_label].shape[0] - max_samples
+    return train_indices, test_indices
+
+
+def shuffle(data: pd.DataFrame, label: pd.Series, shuffle_seed: int | None = None) -> tuple[pd.DataFrame, pd.Series]:
+    """Shuffle data and labels.
+
+    Args:
+        data: Data.
+        label: Labels.
+        shuffle_seed: Seed for shuffling.
+
+    Returns:
+        Tuple containing shuffled data and corresponding labels
+    """
+    if shuffle_seed is not None:
+        data_shape = data.shape
+        data_index = data.index
+
+        # shuffle data before selecting samples
+        data = data.sample(frac=1, random_state=shuffle_seed)
+        label = label.loc[data.index]
+
+        assert data.index.equals(label.index)
+        assert data.shape[0] == label.shape[0]
+        assert data_shape == data.shape
+        assert not data_index.equals(data.index)
+    return data, label
+
+
+def load_train_test_data_for_standardized_sample_size(meta_data_dict: dict) -> tuple[pd.DataFrame, pd.DataFrame]:
     """Load remaining test data from standardized train sample size.
 
     The data is loaded and parsed from the internet (see mltb2.data). The function selects all samples but the first 15
-     of each class as test data.
+     of each class as test data. If the shuffle_seed is set, the data is shuffled before selecting the samples.
 
     Args:
-        data_name: Name of the data to load. Possible options are "colon", "prostate", "leukemia_big".
+        meta_data_dict: The metadata related to the dataset and experiment.
+            Must contain the key "data_name" and "shuffle_seed". Data name must be "colon", "prostate" or
+            "leukemia_big". "shuffle_seed" is an integer or None.
 
     Returns:
-        Dataframe containing labels and data.
+        Tuple containing train data and hold out set as test data.
     """
     # generate function from string
-    load_data_function = globals()[f"load_{data_name}"]
+    load_data_function = globals()[f"load_{meta_data_dict['data_name']}"]
 
     # standardize sample size to balanced data
     label, data = load_data_function()
 
-    # find remaining samples for each class
-    indices: list[int] = []
-    counter_0 = 0
-    counter_1 = 0
+    # shuffle data before selecting samples, if shuffle_seed is set
+    data, label = shuffle(data, label, meta_data_dict["shuffle_seed"])
 
-    for i in range(label.shape[0]):
-        # select samples with label 0
-        if label.iloc[i] == 0:
-            counter_0 += 1
-            if counter_0 > 15:
-                indices.append(i)
+    train_indices_0, test_indices_0 = get_indices_for_class(label, 0, 15)
+    train_indices_1, test_indices_1 = get_indices_for_class(label, 1, 15)
 
-        # select samples with label 1
-        if label.iloc[i] == 1:
-            counter_1 += 1
-            if counter_1 > 15:
-                indices.append(i)
+    train_indices = train_indices_0 + train_indices_1
+    test_indices = test_indices_0 + test_indices_1
 
-    assert len(indices) == data.shape[0] - 30, f"{len(indices)} != {data.shape[0]} - 30"
-    test_data_df = data.iloc[indices]
-    y_test = label.iloc[indices]
+    assert len(test_indices) == data.shape[0] - 30, f"{len(test_indices)} != {data.shape[0]} - 30"
+    test_data_df = data.iloc[test_indices]
+    y_test = label.iloc[test_indices]
     assert test_data_df.shape[0] == data.shape[0] - 30, f"{test_data_df.shape[0]} != {data.shape[0] - 30}"
     assert len(y_test) == test_data_df.shape[0]
     assert test_data_df.shape[1] == data.shape[1]
 
-    test_data_df = convert_to_single_df(test_data_df, y_test)
-    x, y = standardize_sample_size(data, label)
-    train_data_df = convert_to_single_df(x, y)
+    assert len(train_indices) == 30
+    train_data_df = data.iloc[train_indices]
+    y_train = label.iloc[train_indices]
+    assert train_data_df.shape[0] == 30
+    assert len(y_train) == 30
+    assert train_data_df.shape[1] == data.shape[1]
 
-    assert train_data_df.shape[1] == test_data_df.shape[1]
+    test_data_df = convert_to_single_df(test_data_df, y_test)
+    train_data_df = convert_to_single_df(train_data_df, y_train)
+    assert test_data_df.columns[0] == "label"
+    assert train_data_df.columns[0] == "label"
+    assert train_data_df["label"].value_counts().tolist() == [
+        15,
+        15,
+    ], f"{train_data_df['label'].value_counts().tolist()}"
+    assert train_data_df.shape[1] == test_data_df.shape[1] == data.shape[1] + 1  # label column
     assert train_data_df.shape[0] + test_data_df.shape[0] == data.shape[0]
 
-    return test_data_df, train_data_df
+    return train_data_df, test_data_df
 
 
-def standardize_sample_size(data, label) -> tuple[pd.DataFrame, pd.Series]:
-    """Reduce samples to 15 for each class.
-
-    Returns:
-        Tuple containing balanced data and corresponding labels.
-    """
-    # reduce samples to 15 for each class
-    indices: list[int] = []
-    for i in range(label.shape[0]):
-        if label.iloc[i] == 0:
-            indices.append(i)
-        if len(indices) == 15:
-            break
-
-    for i in range(label.shape[0]):
-        if label.iloc[i] == 1:
-            indices.append(i)
-        if len(indices) == 30:
-            break
-
-    data = data.iloc[indices]
-    label = label.iloc[indices]
-    assert data.shape[0] == 30
-    assert len(label) == 30
-    return data, label
-
-
-def get_data_df(
+def load_data_df(
     meta_data_dict: dict, generate_random_noise: bool = False, path_for_random_noise: str = ""
 ) -> pd.DataFrame:
     """Load data for the experiment.
@@ -168,6 +205,7 @@ def get_data_df(
     else:
         # load data
         assert meta_data_dict["data_name"] in ["colon", "prostate", "leukemia_big"], "Invalid data name."
-        data_df = load_data_with_standardized_sample_size(meta_data_dict["data_name"])
+        data_df, _ = load_train_test_data_for_standardized_sample_size(meta_data_dict)
+        assert data_df.shape[0] == 30, f"Number of samples is not 30: {data_df.shape[0]}"
     return data_df
     # return pd.read_csv(f"../../data/artificial_biomarker_data_2.csv").iloc[:, list(range(10)) + list(range(-7, 0))]
