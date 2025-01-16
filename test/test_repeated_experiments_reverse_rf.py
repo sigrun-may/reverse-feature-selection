203af6d424229afe547dad179740900d1bdf18cf
Sigrun May <sigrun.may@gmail.com>
2025-01-16 10:49:24 +0100

    Refactor and cleanup

--- test/test_repeated_experiments_reverse_rf.py
+++ test/test_repeated_experiments_reverse_rf.py
@@ -6,7 +6,6 @@
 
 """Main test script for cacluating a grid of repeated feature selection experiments for reverse feature selection."""
 
-from unittest.mock import MagicMock, patch
 
 from feature_selection_benchmark.repeated_experiments_reverse_rf import define_random_seeds, main
 
@@ -21,21 +20,3 @@ def test_define_random_seeds():
     flattened_seeds = [seed for sublist in seeds for seed in sublist]
     assert len(flattened_seeds) == 90
     assert len(set(flattened_seeds)) == 90
-
-
-@patch("feature_selection_benchmark.repeated_experiments_reverse_rf.pickle.dump")
-@patch("feature_selection_benchmark.repeated_experiments_reverse_rf.cross_validation.cross_validate")
-@patch("feature_selection_benchmark.repeated_experiments_reverse_rf.load_data_df")
-@patch("feature_selection_benchmark.repeated_experiments_reverse_rf.Path.mkdir")
-@patch("feature_selection_benchmark.repeated_experiments_reverse_rf.sys.argv", new=["script_name", "../results"])
-def test_main(mock_mkdir, mock_load_data_df, mock_cross_validate, mock_pickle_dump):
-    """Test main function."""
-    mock_load_data_df.return_value = MagicMock(shape=(30, 2001))
-    mock_cross_validate.return_value = MagicMock()
-
-    main()
-
-    mock_mkdir.assert_called_once_with(parents=True, exist_ok=True)
-    mock_load_data_df.assert_called()
-    mock_cross_validate.assert_called()
-    mock_pickle_dump.assert_called()
