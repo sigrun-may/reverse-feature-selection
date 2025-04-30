2de4cfe6251ddd0109783adc160a7a9f8cc504da
Sigrun May <sigrun.may@gmail.com>
2025-04-30 12:59:49 +0200

    Bugfix logging

--- feature_selection_benchmark/cross_validation.py
+++ feature_selection_benchmark/cross_validation.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import LeaveOneOut
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def cross_validate(data_df: pd.DataFrame, meta_data: dict, feature_selection_function) -> list[pd.DataFrame]:
@@ -30,7 +31,7 @@ def cross_validate(data_df: pd.DataFrame, meta_data: dict, feature_selection_fun
     cv_result_list = []
     loo = LeaveOneOut()
     for fold_index, (train_indices, _) in enumerate(loo.split(data_df)):
-        logging.info(f"fold_index {fold_index + 1} of {data_df.shape[0]}")
+        logger.info(f"fold_index {fold_index + 1} of {data_df.shape[0]}")
         # Calculate raw values for calculating feature subsets
         selected_feature_subset = feature_selection_function(data_df, train_indices, meta_data)
         cv_result_list.append(selected_feature_subset)
